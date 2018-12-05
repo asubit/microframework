a10b04c2dfcd61330f5a1a45368c75737f95ab0b
Aymen Mechiche <aymen.mechiche.ext@gfi.fr>
2018-12-05 14:50:23 +0100

    [data] page store data

--- App/App.php
+++ App/App.php
@@ -8,26 +8,28 @@ class App {
 
     public function __construct($env = 'dev') {
         // Init asubit microframework app
-        $this->init($this->env);
+        $this->env = $env;
     }
 
     /*
      * Init asubit microframework app
      */
-    public function init($env) {
-        switch ($env) {
+    public function init() {
+        switch ($this->env) {
             case 'dev':
                 error_reporting(E_ALL);
                 ini_set('display_errors', 1);
-                echo $this->getDebugBar();
+                echo $this->debugBar();
                 break;
 
             default:
                 break;
         }
     }
-
-    public function getDebugBar() {
-        return '<div style="position: fixed;bottom: 0px;width: 100%; background: #eee; border-top: black 1px solid;">dev</div>';
+    
+    public function debugBar() {
+        $content = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $debugBar = '<div style="position:fixed;bottom:0px;width:100%;background:#eee;border-top:black 1px solid;">'.$content.'</div>';
+        return $debugBar;
     }
 }
--- index.php
+++ index.php
@@ -3,7 +3,13 @@
 include('App/App.php');
 include('App/Routing.php');
 
+// Init the Asubit Microframework App
 $app = new App('dev');
+//$app->init();
 
+// Instanciate routing
 $routing = new Routing();
+// Match the current route URL and call associated controller class
 $routing->go();
+
+$app->init();
